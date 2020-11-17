40479b1812c7ffda836d65b6581247a668e4a273
Ulazka Mateusz <ulazkamateusz@gmail.com>
2020-11-17 21:50:21 +0100

    Lab5

---
+++ Zajecia/Lab3/Zad2/DiabelskiMlyn.java
@@ -0,0 +1,28 @@
+package Zad2;
+
+public class DiabelskiMlyn {
+    private int iloscMiejsc;
+    private int cena;
+
+    DiabelskiMlyn(int iloscMiejsc, int cena) {
+        this.iloscMiejsc = iloscMiejsc;
+        this.cena = cena;
+    }
+
+    public synchronized boolean mamyMiejsce() {
+        if (iloscMiejsc > 0) {
+            iloscMiejsc--;
+            return true;
+        }
+
+        return false;
+    }
+
+    public synchronized void wyjdz() {
+        iloscMiejsc++;
+    }
+
+    public int cenaWejscia() {
+        return cena;
+    }
+}
